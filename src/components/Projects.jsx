ea4f2f73e45d74efe9897cfa549950910028220f
Ayush Soni <soni.ayush.2212@gmail.com>
2025-05-15 02:24:16 +0530

    Change text color for visibility

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,8 +23,8 @@ const Projects = () => {
           transition={{ duration: 0.5 }}
         >
           <div className="max-w-[750px] px-10 py-5 rounded text-white">
-            <h1 className="text-3xl font-bold text-slate-100">Reddit</h1>
-            <p className="mt-5 text-lg font-medium text-gray-300">
+            <h1 className="text-3xl font-bold text-gray-600">Reddit</h1>
+            <p className="mt-5 text-lg font-medium text-gray-500">
               A clone of reddit that incorporates all the major instead the
               actual app. It has an amazing UI which is mobile responsive and
               uses latest Chakra UI components to model different parts, this is
@@ -54,8 +54,8 @@ const Projects = () => {
             </GlareCard>
           </div>
           <div className="max-w-[750px] py-5 rounded text-white">
-            <h1 className="text-3xl font-bold text-slate-100">Lobby</h1>
-            <p className="mt-5 text-lg font-medium text-gray-300">
+            <h1 className="text-3xl font-bold text-gray-600">Lobby</h1>
+            <p className="mt-5 text-lg font-medium text-gray-500">
               I wanted to learn how discord works and so I created Lobby. I used
               WebSockets and other browser APIs to let users talk to each other
               using their voice as well as through text messages.
@@ -74,8 +74,8 @@ const Projects = () => {
           transition={{ duration: 0.5 }}
         >
           <div className="max-w-[750px] px-10 py-5 rounded text-white">
-            <h1 className="text-3xl font-bold text-slate-100">Querify</h1>
-            <p className="mt-5 text-lg font-medium text-gray-300">
+            <h1 className="text-3xl font-bold text-slate-600">Querify</h1>
+            <p className="mt-5 text-lg font-medium text-gray-500">
               This is my final university project where we tried to make life
               easier for people who don't know how to interact with databases
               using query languages. I used NLP to talk to the user and generate
@@ -93,18 +93,6 @@ const Projects = () => {
             </GlareCard>
           </div>
         </motion.div>
-        <div className="flex justify-center items-center h-[20rem] flex-col px-4">
-          <p className="text-slate-200 dark:text-neutral-400 text-xl md:text-5xl max-w-5xl text-left mb-10">
-            I make a lot of stuff so visit my{"  "}
-            <LinkPreview
-              url="https://github.com/SpectreFury"
-              className="font-bold bg-clip-text text-transparent bg-gradient-to-br from-purple-500 to-pink-500"
-            >
-              Github
-            </LinkPreview>{" "}
-            to view my other projects.
-          </p>
-        </div>
       </div>
     </section>
   );
